4f66a7b41e06998081c1d71be7f09e5a1242517f
Nechityalo <137058334+Nechityalo@users.noreply.github.com>
2025-06-10 14:00:12 +0300

    Create timeline.stories.js

---
+++ src/components/Timeline/timeline.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';Add commentMore actions
+import { storiesOf, action } from '@kadira/storybook';
+
+import Timeline from '../components/Timeline/Timeline';
+import timelineFixture from '../../tests/__fixtures__/timeline';
+import Timeline from './Timeline';
+import timelineFixture from './timeline.fixtures';
+
+storiesOf('Timeline', module)
+  .add('today', () => (
+    <Timeline
+      days={timelineFixture}
+      selectedDay={timelineFixture[timelineFixture.length - 1]}
+      onDaySelect={action('day selected')}
+    />
+  ))
+  .add('middle', () => (
+    <Timeline
+      days={timelineFixture}
+      selectedDay={timelineFixture[2]}
+      onDaySelect={action('day selected')}
+    />
+  ))
+  .add('first day', () => (
+    <Timeline
+      days={timelineFixture}
+      selectedDay={timelineFixture[0]}
+      onDaySelect={action('day selected')}
+    />
+  ))
+  .add('one day', () => (
+    <Timeline
+      days={timelineFixture.slice(-1)}
+      selectedDay={timelineFixture[timelineFixture.length - 1]}
+      onDaySelect={action('day selected')}
+    />
+  ));
