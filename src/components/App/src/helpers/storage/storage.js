49d9f3e08a22578a2f07d86859ec5d58855db0dc
Nechityalo <137058334+Nechityalo@users.noreply.github.com>
2025-06-10 13:31:16 +0300

    Create storage.js

---
+++ src/components/App/src/helpers/storage/storage.js
@@ -0,0 +1,24 @@
+import { STORAGE_KEY, INITIAL_STATE } from '../constants';Add commentMore actions
+import { STORAGE_KEY, INITIAL_STATE } from '../../constants';
+
+export function initialize() {
+  const data = get();
+
+  if (!data) {
+    set(INITIAL_STATE);
+  }
+}
+
+export function get() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch(e) {
+    return null;
+  }
+}
+
+export function set(data) {
+  return localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
