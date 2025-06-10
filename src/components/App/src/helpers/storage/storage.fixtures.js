342d323bdb916cce3878af6cbb24850f6530e626
Nechityalo <137058334+Nechityalo@users.noreply.github.com>
2025-06-10 13:30:17 +0300

    Create storage.fixtures.js

---
+++ src/components/App/src/helpers/storage/storage.fixtures.js
@@ -0,0 +1,39 @@
+import format from 'date-fns/format';Add commentMore actions
+import { STORAGE_KEY, DATE_KEY_FORMAT, INITIAL_STATE } from '../../src/constants';
+import { STORAGE_KEY, DATE_KEY_FORMAT, INITIAL_STATE } from '../../constants';
+
+export const empty = {
+  [STORAGE_KEY]: null,
+};
+
+export const malformed = {
+  [STORAGE_KEY]: 'malformed JSON',
+};
+
+export const initial = {
+  [STORAGE_KEY]: JSON.stringify(INITIAL_STATE),
+};
+
+export const defaultDateInPast = new Date(2017, 3, 22);
+export const defaultDateNow = new Date(2017, 3, 24);
+
+const dateKey = format(defaultDateNow, DATE_KEY_FORMAT);
+export const oneDay = {
+  [STORAGE_KEY]: JSON.stringify(
+    Object.assign(
+      {},
+      INITIAL_STATE,
+      {
+        consumption: {
+          [dateKey]: [{
+            type: 'water',
+            amount: 0.3,
+          }, {
+            type: 'tea',
+            amount: 0.5,
+          }],
+        },
+      }
+    )
+  ),
+}
