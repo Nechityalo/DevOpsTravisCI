b51713c4d28df3d502dda441996760957609b971
Nechityalo <137058334+Nechityalo@users.noreply.github.com>
2025-06-10 13:32:11 +0300

    Create storage.test.js

---
+++ src/components/App/src/helpers/storage/storage.test.js
@@ -0,0 +1,37 @@
+import * as storage from '../../src/helpers/storage';Add commentMore actions
+import * as storage from './storage';
+
+import { STORAGE_KEY, INITIAL_STATE } from '../../src/constants';
+import { STORAGE_KEY, INITIAL_STATE } from '../../constants';
+
+import { getLocalStorageStub } from '../__mocks__/localStorage';
+import { malformed, empty } from '../__fixtures__/consumption';
+import { malformed, empty } from './storage.fixtures.js';
+
+describe('storage', () => {
+  beforeAll(() => {
+    global.localStorage = getLocalStorageStub();
+  })
+
+  describe('get', () => {
+    it('should return empty object on malformed json in localStorage', () => {
+      global.localStorage.setFixture(malformed);
+
+      const data = storage.get();
+      expect(data).toBe(null);
+    })
+  });
+
+  describe('initialize', () => {
+    it('should properly initialize localStorage with initial value', () => {
+      global.localStorage.setFixture(empty);
+
+      storage.initialize();
+
+      const str = global.localStorage.getItem(STORAGE_KEY);
+      const data = JSON.parse(str);
+
+      expect(data).toEqual(INITIAL_STATE);
+    });
+  });
+});
