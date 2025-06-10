0c443d5b77161f6d27ec8422bd00a2e3bc7b270d
Nechityalo <137058334+Nechityalo@users.noreply.github.com>
2025-06-10 13:25:24 +0300

    Update Timeline.test.js

--- src/components/App/src/components/App/__snapshots__/src/components/Timeline/Timeline.test.js
+++ src/components/App/src/components/App/__snapshots__/src/components/Timeline/Timeline.test.js
@@ -52,3 +52,109 @@ describe('Timeline', () => {
     const root = document.createElement('main');
     const render = () => ReactDOM.render((
       <Timeline
+        days={timelineFixture}
+        selectedDay={timelineFixture[timelineFixture.length - 1]}
+      />
+    ), root);
+
+    expect(render).not.toThrow();
+  });
+
+  it('should contain 3 entries', () => {
+    const timeline = render(
+      <Timeline
+        days={timelineFixture}
+        selectedDay={timelineFixture[3]}
+      />
+    );
+
+    const selector = timeline.find('.timeline__day');
+
+    expect(selector.length).toBe(3);
+  });
+
+  it('should render dummy records before first entry and after last entry', () => {
+    const timeline = render(
+      <Timeline
+        days={timelineFixture.slice(0, 1)}
+        selectedDay={timelineFixture[0]}
+      />
+    );
+
+    const selector = timeline.find('.timeline__day');
+    const dummyTest = /timeline__day--dummy/;
+
+    expect(selector[0].attribs['class']).toMatch(dummyTest);
+    expect(selector[1].attribs['class']).not.toMatch(dummyTest);
+    expect(selector[2].attribs['class']).toMatch(dummyTest);
+  });
+
+  it('should call onDaySelect on left swipe gesture', () => {
+    const selectDayMock = jest.fn();
+
+    const timeline = mount(
+      <Timeline
+        days={timelineFixture}
+        selectedDay={timelineFixture[3]}
+        onDaySelect={selectDayMock}
+      />
+    );
+
+    const selector = timeline.find('.timeline');
+    simulateSwipe(selector, 'left');
+
+    expect(selectDayMock).toBeCalled();
+    expect(selectDayMock).toBeCalledWith(timelineFixture[4]);
+  });
+
+  it('should call onDaySelect on right swipe gesture', () => {
+    const selectDayMock = jest.fn();
+
+    const timeline = mount(
+      <Timeline
+        days={timelineFixture}
+        selectedDay={timelineFixture[3]}
+        onDaySelect={selectDayMock}
+      />
+    );
+
+    const selector = timeline.find('.timeline');
+    simulateSwipe(selector, 'right');
+
+    expect(selectDayMock).toBeCalled();
+    expect(selectDayMock).toBeCalledWith(timelineFixture[2]);
+  });
+
+  it('should not call onDaySelect on swipe gestures when entry is either first or last', () => {
+    const selectDayMock = jest.fn();
+
+    const timeline = mount(
+      <Timeline
+        days={timelineFixture.slice(0,1)}
+        selectedDay={timelineFixture[0]}
+        onDaySelect={selectDayMock}
+      />
+    );
+
+    const selector = timeline.find('.timeline');
+    simulateSwipe(selector, 'right');
+    simulateSwipe(selector, 'left');
+
+    expect(selectDayMock).not.toBeCalled();
+  });
+
+  it('should display date in proper format', () => {
+    const timeline = mount(
+      <Timeline
+        days={timelineFixture}
+        selectedDay={timelineFixture[timelineFixture.length - 2]}
+      />
+    );
+
+    const selector = timeline.find('.timeline__date');
+
+    expect(selector.at(2).text()).toMatch('Today');
+    expect(selector.at(1).text()).toMatch('Yesterday');
+    expect(selector.at(0).text()).toMatch(/\d{2} \w{3}/);
+  });
+});
